4f3981f4f86d8ba487596dcd5e09b8b1fae02823
arthurcarvalho88-prog <arthur.carvalho88@gmail.com>
2026-04-23 13:18:24 -0400

    feat: add /privacidade route

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import LandingPage from "@/pages/LandingPage";
+  import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+  import { Toaster } from "@/components/ui/toaster";
+  import { TooltipProvider } from "@/components/ui/tooltip";
+  import NotFound from "@/pages/not-found";
+  import LandingPage from "@/pages/LandingPage";
+  import PrivacyPolicy from "@/pages/PrivacyPolicy";
 
-const queryClient = new QueryClient();
+  const queryClient = new QueryClient();
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={LandingPage} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
+  function Router() {
+    return (
+      <Switch>
+        <Route path="/" component={LandingPage} />
+        <Route path="/privacidade" component={PrivacyPolicy} />
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
 
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
-          <Router />
-        </WouterRouter>
-        <Toaster />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
+  function App() {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <WouterRouter base="">
+            <Router />
+          </WouterRouter>
+          <Toaster />
+        </TooltipProvider>
+      </QueryClientProvider>
+    );
+  }
 
-export default App;
+  export default App;
+  
